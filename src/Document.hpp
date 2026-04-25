a4b60bac87e0ef84635dd51e4ceb151cfbfddf6d
Vikhireva_Maria <m.vihireva@gmail.com>
2026-04-25 12:22:24 +0300

    Masha1

--- src/Document.hpp
+++ src/Document.hpp
@@ -2,18 +2,18 @@
 #include <string>
 namespace lab5::document_work
 {
-    class Document
-    {
-    public:
-        Document(size_t id, const std::string& name, const std::string& text);
+class Document
+{
+  public:
+    Document(size_t id, const std::string& name, const std::string& text);
 
-        size_t getId() const;
-        const std::string& getName() const;
-        const std::string& getText() const;
+    size_t getId() const;
+    const std::string& getName() const;
+    const std::string& getText() const;
 
-    private:
-        size_t id_;
-        std::string name_;
-        std::string text_;
-    };
+  private:
+    size_t id_;
+    std::string name_;
+    std::string text_;
+};
 } // namespace lab5::document_work
